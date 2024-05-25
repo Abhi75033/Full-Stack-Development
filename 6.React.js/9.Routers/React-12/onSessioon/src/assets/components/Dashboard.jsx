7bc9fadac8e52c794c12ed4fc68dbbf5fd212b3b
Abhi75033 <abhishekyadav1112.21@gmail.com>
2024-05-25 18:36:29 +0530

    new project

---
+++ 6.React.js/9.Routers/React-12/onSessioon/src/assets/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+function Dasboard() {
+    return ( 
+    <div className="container">
+        <h1>Dasboard</h1>
+<p><a className="link-offset-2 link-underline link-underline-opacity-0" href="#">Underline opacity 0</a></p>
+<p><a className="link-offset-2 link-underline link-underline-opacity-10" href="#">Underline opacity 10</a></p>
+<p><a className="link-offset-2 link-underline link-underline-opacity-25" href="#">Underline opacity 25</a></p>
+<p><a className="link-offset-2 link-underline link-underline-opacity-50" href="#">Underline opacity 50</a></p>
+<p><a className="link-offset-2 link-underline link-underline-opacity-75" href="#">Underline opacity 75</a></p>
+<p><a className="link-offset-2 link-underline link-underline-opacity-100" href="#">Underline opacity 100</a></p>
+    </div> 
+    );
+}
+
+export default Dasboard;
